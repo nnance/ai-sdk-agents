16a5a6e3087cb79e5ceab9ac8de40e40fc332b14
Nick Nance <nance.nick@gmail.com>
2025-01-29 06:27:04 -0800

    real tools test

--- src/tools/index.test.ts
+++ src/tools/index.test.ts
@@ -1,27 +1,28 @@
 import { installMatchers } from "../util/matchers";
+import { calculate } from "./math";
+import { registry } from "../util/registry";
+
+const model = registry.languageModel("remoteProvider:structure-large");
 
 installMatchers();
 
 describe("semantic similarity tests", () => {
-  test("should pass when strings are semantically similar", async () =>
-    expect("The quick brown fox").toMatchSemanticSimilarity(
-      "A fast brown fox"
-    ));
+  test("should pass when strings are semantically similar", async () => {
+    const { toolCalls } = await calculate(
+      model,
+      "A taxi driver earns $9461 per 1-hour of work. " +
+        "If he works 12 hours a day and in 1 hour " +
+        "he uses 12 liters of petrol with a price  of $134 for 1 liter. " +
+        "How much money does he earn after expenses in one day?"
+    );
 
-  test("should fail when strings are not semantically similar", async () =>
-    expect("The quick brown fox").not.toMatchSemanticSimilarity(
-      "The weather is nice today"
-    ));
+    const answer = toolCalls.find((call) => call.toolName === "answer");
+    if (!answer) {
+      throw new Error("Answer tool call not found");
+    }
 
-  test("should pass when strings are semantically similar with custom threshold", async () =>
-    expect("The quick brown fox").toMatchSemanticSimilarity(
-      "A fast brown fox",
-      0.7
-    ));
-
-  test("should fail when strings are not semantically similar with custom threshold", async () =>
-    expect("The quick brown fox").not.toMatchSemanticSimilarity(
-      "The weather is nice today",
-      0.9
-    ));
+    return expect(answer.args.answer).toMatchSemanticSimilarity(
+      "earns $94,236 after expenses per day."
+    );
+  }, 10000);
 });
